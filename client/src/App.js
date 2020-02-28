460bfce5d18176d9aeccd557347d4cf47005779d
Alyssa Saez <xcthaeh@gmail.com>
2020-02-28 03:58:34 +0200

    Added some basic functionality, visuals, and reducers

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import Player from './features/player'
+import handleMovement from './features/movement'
+import { MAP_HEIGHT, MAP_WIDTH } from './config/constants'
+import './App.css'
+import ArrowKeys from "./arrowkeys.png"
 
-class App extends Component {
-  render() {
 
-    return (
-      <div>
-        <Player />
-      </div>
-    )
-  }
+
+function App() {
+  return (
+    <div
+      className="map"
+      style={{
+        height: MAP_HEIGHT,
+        width: MAP_WIDTH,
+      }}
+    >
+      <Player />
+      <img className="arrowsKeys" src={ArrowKeys} alt="Arrow keys" />
+    </div>
+  )
 }
 
-export default App;
+
+export default handleMovement(App);
--- client/src/config/store.js
+++ client/src/config/store.js
@@ -1,5 +1,6 @@
 import { createStore, combineReducers } from 'redux'
 import playerReducer from "../features/player/reducer"
+
 const rootReducer = combineReducers({
     player: playerReducer,
 })
@@ -8,3 +9,5 @@ const store = createStore(
     rootReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+export default store
