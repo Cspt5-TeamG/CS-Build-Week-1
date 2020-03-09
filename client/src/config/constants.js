2dabb30b6c25f5464620d9e47fb2dbea6d8c4c8d
Cedricium <amaya.cedric@gmail.com>
2020-03-08 21:06:54 -0700

    Connect API communication with Redux

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,3 +1,12 @@
 export const MAP_WIDTH = 800
 export const MAP_HEIGHT = 420
 export const PLAYER_SIZE = 10
+
+// API-specific
+export const BASE_API_URL =
+  process.env.REACT_APP_BASE_API_URL || `http://localhost:8000/api`
+
+export const REGISTRATION_ENDPOINT = `${BASE_API_URL}/registration/`
+export const LOGIN_ENDPOINT = `${BASE_API_URL}/login/`
+export const INITIALIZE_GAME_ENDPOINT = `${BASE_API_URL}/adv/init/`
+export const ROOMS_ENDPOINT = `${BASE_API_URL}/adv/rooms/`
--- client/src/features/movement/index.js
+++ client/src/features/movement/index.js
@@ -1,34 +1,38 @@
 import store from '../../config/store'
 
 function handleDirctionMove(e, direction) {
-    console.log(`Moving ${direction}!`)
-    store.dispatch({ type: "MOVE_PLAYER", payload: direction })
-    e.preventDefault()
+  const state = store.getState()
+  if (!state.player.isLoggedIn) {
+    return alert('Must log in before playing!')
+  }
+  console.log(`Moving ${direction}!`)
+  store.dispatch({ type: 'MOVE_PLAYER', payload: direction })
+  e.preventDefault()
 }
 
 function handleKeyDown(e) {
-    switch (e.keyCode) {
-        case 40:
-            handleDirctionMove(e, 'down')
-            return
-        case 37:
-            handleDirctionMove(e, 'left')
-            return
-        case 39:
-            handleDirctionMove(e, 'right')
-            return
-        case 38:
-            handleDirctionMove(e, 'up')
-            return
-        default:
-            console.log(e.keyCode)
-    }
+  switch (e.keyCode) {
+    case 40:
+      handleDirctionMove(e, 'down')
+      return
+    case 37:
+      handleDirctionMove(e, 'left')
+      return
+    case 39:
+      handleDirctionMove(e, 'right')
+      return
+    case 38:
+      handleDirctionMove(e, 'up')
+      return
+    default:
+      console.log(e.keyCode)
+  }
 }
 
 export default function handleMovement(wrappedComponent) {
-    window.addEventListener('keydown', (e) => {
-        handleKeyDown(e, wrappedComponent)
-    })
+  window.addEventListener('keydown', e => {
+    handleKeyDown(e, wrappedComponent)
+  })
 
-    return wrappedComponent
+  return wrappedComponent
 }
